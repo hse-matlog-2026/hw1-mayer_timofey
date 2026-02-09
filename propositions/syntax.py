64464a2023f6ae9fa0f934d9e3b00ead12ad80b6
Maiertim <dadasada3812@gmail.com>
2026-02-09 15:17:37 +0300

    Task 1.1–1.2: repr and variables
    Task 1.1–1.2: repr and variables

--- propositions/syntax.py
+++ propositions/syntax.py
@@ -109,6 +109,12 @@ class Formula:
         Returns:
             The standard string representation of the current formula.
         """
+        if is_variable(self.root) or is_constant(self.root):
+            return self.root
+        elif is_unary(self.root):
+            return '~' + repr(self.first)
+        else:
+            return '(' + repr(self.first) + self.root + repr(self.second) + ')'
         # Task 1.1
 
     def __eq__(self, other: object) -> bool:
@@ -145,6 +151,13 @@ class Formula:
         Returns:
             A set of all variable names used in the current formula.
         """
+    if is_variable(self.root):
+        return {self.root}
+    if is_constant(self.root):
+        return set()
+    if is_unary(self.root):
+        return self.first.variables()
+    return self.first.variables() | self.second.variables()
         # Task 1.2
 
     @memoized_parameterless_method
